4ac2476146e0d4ca5fc030a894b1648669074bbc
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-03-14 17:37:12 -0400

    Added routers

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const connection = require("./src/config/db");
+const studentsRouter = require("./src/routes/students");
+const coursesRouter = require("./src/routes/courses");
+const resultsRouter = require("./src/routes/results");
 
 const app = express();
 
-// Connect to the database
+// Connect to MySQL
 connection.connect((err) => {
   if (err) {
     console.log("Error connecting to MySQL:", err);
   }
   console.log("Connected to MySQL");
 });
+
+// Routes
+app.use("/students", studentsRouter);
+app.use("/courses", coursesRouter);
+app.use("/results", resultsRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
