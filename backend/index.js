65d0085b8bfb93871e989bec6c9cf96ae74ae880
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-03-14 17:31:50 -0400

    Setting up Routes and Controllers

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const connection = require("./config/db");
+const connection = require("./src/config/db");
 
 const app = express();
 
