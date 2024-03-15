171131e71d27c3b4d42b1bb182d301eb36e720a8
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-03-14 20:19:47 -0400

    Created Course Router

---
+++ backend/src/routes/courses.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const {
+  getCourses,
+  addCourse,
+  updateCourse,
+  deleteCourse,
+} = require("../controllers/courses");
+
+const router = express.Router();
+
+// Students Routes
+
+router.get("/", getCourses);
+router.post("/", addCourse);
+router.put("/:id", updateCourse);
+router.delete("/:id", deleteCourse);
+
+module.exports = router;
