02ba188afabc7383190742a40d6627dd9c607746
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-03-15 00:57:38 -0400

    Setup

---
+++ frontend/components/StudentForm.jsx
@@ -0,0 +1,8 @@
+import { useRef, useEffect } from "react";
+import styled from "styled-components";
+
+const StudentForm = () => {
+  return <div>StudentForm</div>;
+};
+
+export default StudentForm;
