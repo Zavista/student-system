82131b2301bda370e2cf82be5054beba638b5126
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-03-15 00:19:01 -0400

    Imported Navbar

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,14 +1,12 @@
 import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
   const navigate = useNavigate();
 
   return (
     <>
-      <div onClick={() => navigate("/")}>Home</div>
-      <div onClick={() => navigate("/students")}>Students</div>
-      <div onClick={() => navigate("/courses")}>Courses</div>
-      <div onClick={() => navigate("/results")}>Results</div>
+      <Navbar></Navbar>
     </>
   );
 };
