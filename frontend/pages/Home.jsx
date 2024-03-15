89c2681d2f73467592719fe1b7ffbd87e7e6cc91
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-03-15 00:35:51 -0400

    Added Wrapper and Home content

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,13 +1,30 @@
 import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 200px;
+  font-weight: 800;
+  color: #333333;
+`;
 
 const Home = () => {
-  const navigate = useNavigate();
-
   return (
-    <>
+    <Wrapper>
       <Navbar></Navbar>
-    </>
+      <Container>Home</Container>
+    </Wrapper>
   );
 };
 
