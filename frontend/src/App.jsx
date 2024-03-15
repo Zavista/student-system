de1c186ce00f3a01c5433388f16cf2ad80d2c091
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-03-14 22:59:44 -0400

    Created React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import {}
-
+import React from "react";
+import Home from "../pages/Home";
+import Students from "../pages/Students";
+import Courses from "../pages/Courses";
+import Results from "../pages/Results";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/students" element={<Students></Students>}></Route>
+        <Route path="/courses" element={<Courses></Courses>}></Route>
+        <Route path="/results" element={<Results></Results>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
