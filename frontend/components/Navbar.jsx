64df7fe3780b5ddf38f6e1293ea65b976cd33798
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-03-15 00:18:40 -0400

    Created Navbar

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -1,5 +1,77 @@
+import React from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import SchoolOutlinedIcon from "@mui/icons-material/SchoolOutlined";
+import PeopleAltOutlinedIcon from "@mui/icons-material/PeopleAltOutlined";
+import AssessmentOutlinedIcon from "@mui/icons-material/AssessmentOutlined";
+
+const Container = styled.div`
+  height: 100vh;
+  width: 10%;
+  background-color: #333333;
+  color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Item = styled.div`
+  width: 90%;
+  height: 40px;
+  margin-bottom: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 2px;
+  cursor: pointer;
+  font-size: 20px;
+  font-weight: bold;
+  transition: background-color color 0.3s;
+
+  &:hover {
+    background-color: #aaaaaa;
+  }
+`;
+
+const Top = styled.div`
+  width: 100%;
+  height: 60px;
+  margin-bottom: 8px;
+  background-color: #fff;
+  color: black;
+  font-size: 30px;
+  font-weight: 900;
+  border: 4px solid #333333;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      <Top>Menu</Top>
+      <Item onClick={() => navigate("/")}>
+        <HomeOutlinedIcon style={{ flex: "1" }}></HomeOutlinedIcon>
+        <span style={{ flex: "2" }}>Home</span>
+      </Item>
+      <Item onClick={() => navigate("/students")}>
+        <PeopleAltOutlinedIcon style={{ flex: "1" }}></PeopleAltOutlinedIcon>
+        <span style={{ flex: "2" }}>Students</span>
+      </Item>
+      <Item onClick={() => navigate("/courses")}>
+        <SchoolOutlinedIcon style={{ flex: "1" }}></SchoolOutlinedIcon>
+        <span style={{ flex: "2" }}>Courses</span>
+      </Item>
+      <Item onClick={() => navigate("/results")}>
+        <AssessmentOutlinedIcon style={{ flex: "1" }}></AssessmentOutlinedIcon>
+        <span style={{ flex: "2" }}>Results</span>
+      </Item>
+    </Container>
+  );
 };
 
 export default Navbar;
