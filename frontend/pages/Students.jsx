0a69e25d3212a5baf5e1bade1da5564ba7d7cd2b
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-03-15 00:58:00 -0400

    Add Top to Student

--- frontend/pages/Students.jsx
+++ frontend/pages/Students.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../components/Navbar";
 import styled from "styled-components";
+import StudentForm from "../components/StudentForm";
 
 const Wrapper = styled.div`
   width: 100%;
@@ -11,18 +12,36 @@ const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
-  font-size: 200px;
   font-weight: 800;
   color: #333333;
+  border-left: 10px solid #fff;
+  border-right: 10px solid #fff;
+`;
+
+const Top = styled.div`
+  width: 100%;
+  height: 60px;
+  padding-top: 12px;
+  margin-bottom: 8px;
+  color: #333333;
+  font-size: 32px;
+  font-weight: 900;
+  border-bottom: 2px solid #333333;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Students = () => {
   return (
     <Wrapper>
       <Navbar></Navbar>
-      <Container>Students</Container>
+      <Container>
+        <Top>Students</Top>
+        <StudentForm></StudentForm>
+      </Container>
     </Wrapper>
   );
 };
